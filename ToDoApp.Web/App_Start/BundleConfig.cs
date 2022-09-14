269d39f55d3cf80d309852e1e3acb955c71fc47d
Roy Ramos <royr719@gmail.com>
2022-09-14 17:13:26 -0600

    Refactoring bundles

--- ToDoApp.Web/App_Start/BundleConfig.cs
+++ ToDoApp.Web/App_Start/BundleConfig.cs
@@ -12,25 +12,13 @@ namespace ToDoApp.Web
                 .Include(
                         "~/assets/js/libs/kendo-ui/js/jquery.min.js",
                         "~/assets/js/libs/kendo-ui/js/kendo.all.min.js"
-                        ));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr")
-                .Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-                .Include(
-                      "~/Scripts/bootstrap.js"));
+                ));
 
             bundles.Add(new StyleBundle("~/Content/css")
                 .Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/kendoCss")
-                .Include("~/assets/js/libs/kendo-ui/styles/kendo.default-main.min.css"));
+                      "~/Content/site.css",
+                      "~/assets/js/libs/kendo-ui/styles/kendo.default-main.min.css"));
         }
     }
 }
